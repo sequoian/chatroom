d1c6d2f11c5e1e60b99f3607c1abf1b53a2e03ae
Zach Askins <zach303@msn.com>
2018-02-25 16:21:22 -0800

    message alert button

--- src/components/SendMessage.js
+++ src/components/SendMessage.js
@@ -6,24 +6,27 @@ import './SendMessage.css'
 let SendMessage = ({dispatch}) => {
   let input
   return (
-    <form
-      onSubmit={e => {
-        e.preventDefault()
-        if (input.value.trim()) {
-          dispatch(addMessage(input.value))
-          input.value = ''
-        }
-        document.getElementById('msg-input').focus()
-      }}
-    >
-      <input
-        id="msg-input"
-        ref={node => input = node}
-        placeholder="Message"
-        autoFocus
-      />
-      <button>Send</button>
-    </form>
+    <div className="footer">
+      <form
+        onSubmit={e => {
+          e.preventDefault()
+          if (input.value.trim()) {
+            dispatch(addMessage(input.value))
+            input.value = ''
+          }
+          document.getElementById('msg-input').focus()
+        }}
+      >
+        <input
+          id="msg-input"
+          ref={node => input = node}
+          placeholder="Message"
+          autoFocus
+        />
+        <button>Send</button>
+      </form>
+      <button type="button" id="msg-alert">New Messages</button>
+    </div>
   )
 }
 
--- src/scroll/index.js
+++ src/scroll/index.js
@@ -2,22 +2,34 @@ class ScrollManager {
   constructor() {
     this.scrollPosition = 1
     this.display = document.getElementById('chat-display')
+    this.msgAlert = document.getElementById('msg-alert')
     this.scrollToBottom()
 
     this.display.addEventListener('scroll', () => {
-      // get the scroll percentage of the display
-      this.scrollPosition = this.display.scrollTop / (this.display.scrollHeight - this.display.clientHeight)
+      const scrollPercentage = this.display.scrollTop / (this.display.scrollHeight - this.display.clientHeight)
+      this.scrollPosition = scrollPercentage
+      if (scrollPercentage >= 1) {
+        this.msgAlert.style.visibility = 'hidden'
+      }
     })
 
     this.display.addEventListener('DOMNodeInserted', () => {
       if (this.scrollPosition >= 1) {
         this.scrollToBottom()
       }
+      else {
+        this.msgAlert.style.visibility = 'visible'
+      }
+    })
+
+    this.msgAlert.addEventListener('click', () => {
+      this.scrollToBottom()
     })
   }
 
   scrollToBottom() {
     this.display.scrollTop = this.display.scrollHeight
+    this.msgAlert.style.visibility = 'hidden'
   }
 }
 
